72286a146416365be74f971c2ffe47240e3dbf13
afliw <aflivv@gmail.com>
2017-11-17 23:51:00 +0100

    add cfg

---
+++ cfg/db.php
@@ -0,0 +1,8 @@
+<?php
+
+define("CFG_DB_DRIVER", "mysql");
+define("CFG_DB_HOST", "localhost");
+define("CFG_DB_DBNAME", "name");
+define("CFG_DB_CHARSET", "utf8");
+define("CFG_DB_USER", "user");
+define("CFG_DB_PASSWORD", 'password');
